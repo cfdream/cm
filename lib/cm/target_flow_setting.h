ffe78a79ded6f7bffe5297a969de9d8db9dd4718
cfdream <lxuemei3000@gmail.com>
2015-07-23 15:22:26 -0700

    clear old branch

--- lib/cm/target_flow_setting.h
+++
@@ -1,8 +0,0 @@
-#ifndef TARGET_FLOW_SETTING_H
-#define TARGET_FLOW_SETTING_H
-
-//target flow description
-const double TARGET_LOSS_RATE_THRESHOLD = 0.15;      //target flow loss rate threshold: 0.2
-const double TARGET_VOLUME_THRESHOLD = 20000;       //target flow volume threshold: 20k
-
-#endif
